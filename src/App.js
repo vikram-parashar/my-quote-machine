dcc38a85c90ead788e3159c2f4d675c523489e84
vikram-parashar <work.majorvicky@gmail.com>
2023-07-29 05:56:55 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { quotes } from "./quote";
 
 function App() {
+  const [rnNum, setRnNum] = useState(Math.floor(Math.random() * 4746));
+  const getRandomQuote = () => {
+    setRnNum(Math.floor(Math.random() * 4746));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div
+      className="App"
+      class="flex items-center font-mono justify-center p-10 bg-base h-screen"
+    >
+      <div id="quote-box" class="p-5 rounded-md bg-text max-w-lg md:min-w-lg">
+        <h2 class="text-2xl text-center font-bold mb-5">Quote of the day</h2>
+        <p id="text" class="text-center mb-3">
+          <i class="nf nf-fa-quote_left"></i>
+          {" " + quotes[rnNum].Quote + " "}
+          <i class="nf nf-fa-quote_right"> </i>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div class="flex justify-end mb-3">
+          <span id="author" class="bg-rose right-0 ">
+            -{quotes[rnNum].Author}
+          </span>
+        </div>
+        <hr />
+        <div id="buttons" class="mt-3">
+          <button
+            id="new-quote"
+            onClick={getRandomQuote}
+            class="bg-iris px-3 py-1 rounded-xl"
+          >
+            New Quote
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
