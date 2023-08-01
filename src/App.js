7ae650b61cf3e1ff793eaaef0e79ff9eedacf48a
vikram-parashar <work.majorvicky@gmail.com>
2023-08-02 02:37:42 +0530

    Tweet option added

--- src/App.js
+++ src/App.js
@@ -13,19 +13,18 @@ function App() {
       class="flex items-center font-mono justify-center p-10 bg-base h-screen"
     >
       <div id="quote-box" class="p-5 rounded-md bg-text max-w-lg md:min-w-lg">
-        <h2 class="text-2xl text-center font-bold mb-5">Quote of the day</h2>
+        <h2 class="text-2xl text-center font-bold mb-5">Quote of the day</h2>{" "}
         <p id="text" class="text-center mb-3">
-          <i class="nf nf-fa-quote_left"></i>
-          {" " + quotes[rnNum].Quote + " "}
-          <i class="nf nf-fa-quote_right"> </i>
+          <i class="nf nf-fa-quote_left"></i> {" " + quotes[rnNum].Quote + " "}
+          <i class="nf nf-fa-quote_right"> </i>{" "}
         </p>
         <div class="flex justify-end mb-3">
           <span id="author" class="bg-rose right-0 ">
-            -{quotes[rnNum].Author}
+            - {quotes[rnNum].Author}
           </span>
         </div>
         <hr />
-        <div id="buttons" class="mt-3">
+        <div id="buttons" class="flex justify-between mt-3">
           <button
             id="new-quote"
             onClick={getRandomQuote}
@@ -33,6 +32,17 @@ function App() {
           >
             New Quote
           </button>
+
+          <a
+            id="tweet-quote"
+            href={
+              "https://twitter.com/intent/tweet?text=" + quotes[rnNum].Quote
+            }
+            class="bg-[#1DA1F2] rounded-xl px-3 py-1 text-text"
+            target="_blank"
+          >
+            <i class="nf nf-cod-twitter"></i> Tweet
+          </a>
         </div>
       </div>
     </div>
